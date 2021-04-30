76d597240d17ca0cd01e7c435a6eb9d0b974fac3
Mat Sz <contact@matsz.dev>
2021-04-30 18:08:39 +0200

    added locked to state type

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,14 +1,17 @@
 import { Store } from 'redux';
+import { ActionType } from '../types/ActionType';
 
 import { ActionModel, KeyModel } from '../types/Models';
 
 export interface StateType {
   keys: KeyModel[];
   selectedKeyId?: string;
+  locked: boolean;
 }
 
 let initialState: StateType = {
   keys: [],
+  locked: false,
 };
 
 export type StoreType = Store<StateType, ActionModel>;
@@ -17,6 +20,10 @@ export function applicationState(state = initialState, action: ActionModel) {
   const newState = { ...state };
 
   switch (action.type) {
+    case ActionType.STORE_LOCK:
+      break;
+    case ActionType.STORE_UNLOCK:
+      break;
     default:
       return state;
   }
